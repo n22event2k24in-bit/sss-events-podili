ba0ebda618aa366f58bc3045493f380023ce86f1
Rakesh <mummanarakesh@gmail.com>
2025-05-28 20:12:08 +0530

    add imp routes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mpaFallback from 'vite-plugin-mpa-fallback'; 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(),
+    mpaFallback(),
+    react()],
    base: '/',
 })
